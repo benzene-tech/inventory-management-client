c1a7137fe9e165e862ea3d443c215d7ad55eb32f
Athithya Vidyarth <avidyarth@gmail.com>
2021-05-03 00:15:18 +0530

    Add Git Hooks and ESLint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-console.log("Client Service Up and Running");
+console.log('Client Service Up and Running');
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
